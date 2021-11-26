bba2588d2232e4aaeec0f7f39bd77bd38ed6d9f1
Ibrahim Ismailov <91595645+ibrahimismailov@users.noreply.github.com>
2021-11-26 17:23:25 +0700

    added buttons

--- ColculatorSwift/ViewController.swift
+++ ColculatorSwift/ViewController.swift
@@ -25,44 +25,44 @@ class ViewController: UIViewController {
     var resault:Int = 0
     var newValue:Int = 0
 
-//
-//
-//    @IBAction func onZero(_ sender: Any) {
-//        addDigit(_digit: "0")
-//    }
-//    @IBAction func onOne(_ sender: Any) {
-//        addDigit(_digit: "1")
-//    }
-//    @IBAction func onTwo(_ sender: Any) {
-//        addDigit(_digit: "2")
-//    }
-//    @IBAction func onThree(_ sender: Any) {
-//        addDigit(_digit: "3")
-//    }
-//    @IBAction func onFour(_ sender: Any) {
-//        addDigit(_digit: "4")
-//    }
-//    @IBAction func onFive(_ sender: Any) {
-//        addDigit(_digit: "5")
-//    }
-//    @IBAction func onSix(_ sender: Any) {
-//        addDigit(_digit: "6")
-//    }
-//    @IBAction func onSeven(_ sender: Any) {
-//        addDigit(_digit: "7")
-//    }
-//    @IBAction func onEight(_ sender: Any) {
-//        addDigit(_digit: "8")
-//    }
-//    @IBAction func onNine(_ sender: Any) {
-//        addDigit(_digit: "9")
-//    }
-//    @IBAction func onEqual(_ sender: Any) {
-//
-//        colculate1()
-//    }
-//    @IBAction func onDot(_ sender: Any) {
-//    }
+
+
+    @IBAction func onZero(_ sender: Any) {
+        //addDigit(_digit: "0")
+    }
+    @IBAction func onOne(_ sender: Any) {
+       // addDigit(_digit: "1")
+    }
+    @IBAction func onTwo(_ sender: Any) {
+       // addDigit(_digit: "2")
+    }
+    @IBAction func onThree(_ sender: Any) {
+      //  addDigit(_digit: "3")
+    }
+    @IBAction func onFour(_ sender: Any) {
+       // addDigit(_digit: "4")
+    }
+    @IBAction func onFive(_ sender: Any) {
+       // addDigit(_digit: "5")
+    }
+    @IBAction func onSix(_ sender: Any) {
+       // addDigit(_digit: "6")
+    }
+    @IBAction func onSeven(_ sender: Any) {
+      //  addDigit(_digit: "7")
+    }
+    @IBAction func onEight(_ sender: Any) {
+    //    addDigit(_digit: "8")
+    }
+    @IBAction func onNine(_ sender: Any) {
+   //     addDigit(_digit: "9")
+    }
+    @IBAction func onEqual(_ sender: Any) {
+
+     //   colculate1()
+    }
+    @IBAction func onDot(_ sender: Any) {
+    }
 //
 //    @IBAction func onAdd(_ sender: Any) {
 //        oper = .Add
